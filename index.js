0ed122e66be436a6aed331ed8affbc690474f278
Samantha Ostrowski <isphinxs@hotmail.com>
2021-06-05 20:06:24 -0400

    commit tree 45019fb597fcc0b7e5cee67b0027a9c11497cc6e

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => {
+    const newTitle = title.split(" ").map(word => {
+      const newWord = word.charAt(0).toUpperCase() + word.slice(1);
+      return newWord;
+    }).join(" ");
+    return newTitle;
+  });
 }
